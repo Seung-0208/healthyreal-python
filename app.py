9bb028e1ff078e37b6d6ea3de643f28c399b2612
oseungho <ostraderos@gmail.com>
2024-01-25 09:58:46 +0900

    장소 크롤링 기능 추가

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from api.create_image import CreateImage
+from create_image import CreateImage
+from crawling import areaCrawling
 
 #플라스크 앱 생성
 app = Flask(__name__)
 #CORS에러 처리
-CORS(app)
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 api.add_resource(CreateImage, '/CreateIm')
+api.add_resource(areaCrawling, '/areaCrawling')
+api.decorators=[CORS()]
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
