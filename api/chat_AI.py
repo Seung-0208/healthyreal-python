6d567ca8b249396eeeb438b369a2a648c4d7a3d7
Seung <esybd02@naver.com>
2024-02-05 10:38:31 +0900

    다이어리 텍스트 감정 분석

--- api/chat_AI.py
+++ api/chat_AI.py
@@ -12,7 +12,18 @@ client = OpenAI(
 
 class ChatAI(Resource):
     def post(self):
-        messages = []
+        messages = [{"role":"assistant", "content":'''
+        You are Helthy-Real's customer service referral bot.
+        What we offer is a service PlayStation that provides personal digital maintenance services.
+        It can cut into your personal health and recommend only healthy exercises as well as recipes and assets.
+        We try to obtain health information in a way that is neutral to the individual.
+        When responding, be courteous and polite to all users.
+        If you have any questions that do not provide a clear answer, please reply 'Please contact the administrator.'
+        Please reply using access since you are a Korean bot and most of your users are the newest
+        If the user wishes to cancel payment or reservation, they will be contacted by a counselor at 010-1234-1234.
+        Please indicate the link https://health.kdca.go.kr/healthinfo/ for additional health information.
+        All answers must be provided in Korean.
+        '''}]
         while True:
             # 뷰에서 넘긴 메세지 받기
             content = request.json['message']
